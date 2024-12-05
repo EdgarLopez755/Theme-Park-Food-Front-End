aa28a194981f91d14ac85b0d580497210d84f850
Edgar Lopez <edgarlopez755@gmail.com>
2024-12-04 22:15:51 -0800

    Create dashbaord/landing routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
-// src/App.jsx
+import { useState } from 'react'
+import { Routes, Route } from 'react-router-dom'
+import NavBar from './components/NavBar/NavBar'
+import Landing from './components/Landing/Landing'
+import Dashboard from './components/Dashboard/Dashboard'
 
 const App = () => {
-  return <h1>Hello world!</h1>;
-};
+  const [user, setUser] = useState(null)
 
-export default App;
+  return (
+    <>
+      <NavBar user={user} />
+      <Routes>
+        { user ? (
+          <Route path="/" element={<Dashboard user={user} />} />
+        ) : (
+          <Route path='/' element={<Landing />} />
+        
+        )}
+      </Routes>
+    </>
+  )
+}
+
+export default App
