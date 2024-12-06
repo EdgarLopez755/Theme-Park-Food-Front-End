def433f284ffac60edfd32458a53501b70dd2aca
Edgar Lopez <edgarlopez755@gmail.com>
2024-12-05 23:33:38 -0800

    Add useEffect for food deatails

--- src/services/foodService.js
+++ src/services/foodService.js
@@ -12,4 +12,15 @@ const index = async () => {
     }
 }
 
-export { index }
+const show = async (foodId) => {
+    try {
+        const res = await fetch(`${BASE_URL}/${foodId}`, {
+            headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
+        })
+        return res.json()
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export { index, show }
